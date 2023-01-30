606a6eb5e647742782d3a80b4d52bbea393debaf
Ansh Kapoor <ansh605@gmail.com>
2023-01-31 04:21:33 +0530

    Added Home Page part

---
+++ src/Navbar.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div id="header">
+        <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-dark">
+                <div className="navbar-brand d-block d-lg-none" href="#">
+                    <a href="#"><h2 className="logo"><strong>A</strong>nsh</h2></a>
+                </div>
+                <button className="navbar-toggler navbar_toggle" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav">
+                         <div className="navbar-brand d-none d-lg-block" href="#">
+                        <a href="#"><h2 className="logo center-logo"><strong>a</strong>nsh</h2></a>
+                    </div>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About me</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Education</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Projects</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Work Experience</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Hobbies</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                   
+                    <ul className="navbar-nav">
+                        
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
+
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+import hello from '../hello.gif';
+import designer from '../designer.png'
+import coding from '../coding.png'
+const Home = () => {
+    const imgheight = "80px";
+    return (
+        <div className={`col-md-12 col-sm-12 ${styles.center}`}>
+            <strong className={styles.greeting}>Hello <img src={hello} height={imgheight} /></strong>
+            <div className={styles.intro}>This is Ansh Kapoor</div>
+            <div className={styles.qualification}>Engineer <img src={require("../developer.png")} height={imgheight} /> Designer <img src={designer} height={imgheight} />and Developer <img src={coding} height={imgheight} /></div>
+        </div>
+    )
+}
+
+export default Home
