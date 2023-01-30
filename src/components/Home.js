48236341a15b262396bca7f898cfe18867ab5743
Ansh Kapoor <ansh605@gmail.com>
2023-01-31 04:38:53 +0530

    adjusting margins

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,11 +6,16 @@ import coding from '../coding.png'
 const Home = () => {
     const imgheight = "80px";
     return (
-        <div className={`col-md-12 col-sm-12 ${styles.center}`}>
+        <div className='container'>
+            <div className='row'>
+            <div className={`col-md-12 col-sm-12 ${styles.center}`}>
             <strong className={styles.greeting}>Hello <img src={hello} height={imgheight} /></strong>
             <div className={styles.intro}>This is Ansh Kapoor</div>
             <div className={styles.qualification}>Engineer <img src={require("../developer.png")} height={imgheight} /> Designer <img src={designer} height={imgheight} />and Developer <img src={coding} height={imgheight} /></div>
         </div>
+            </div>
+        </div>
+        
     )
 }
 
